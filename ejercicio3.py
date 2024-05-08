34c657cb5ad41311b7a4f3447cb7eeb4560b737b
gustavoloyola <gustavoloyola287@gmail.com>
2024-05-08 12:14:49 -0300

    commit ejercicio 3

---
+++ ejercicio3.py
@@ -0,0 +1,7 @@
+#calcular area de un triangulo area=(base*altura)/2
+print("Calcúlo del Área de un triángulo")
+
+base = float(input("Ingrese la base del triángulo: "))
+altura = float(input("Ingrese la altura del triángulo: "))
+area = (base * altura)/2
+print("El área del triángulo es: ", area)
