3ce7ec54f4c8bb1ef794de157c2124034e3149df
gustavoloyola <gustavoloyola287@gmail.com>
2024-05-13 02:18:11 -0300

    commit camel case

--- ejercicio4.py
+++ ejercicio4.py
@@ -2,5 +2,5 @@
 frase = input("Ingrese una frase: ")
 
 palabra = frase.split()
-numpalabras = len(palabra)
-print("La cantidad de palabras es: ", numpalabras)
+numPalabras = len(palabra)
+print("La cantidad de palabras es: ", numPalabras)
