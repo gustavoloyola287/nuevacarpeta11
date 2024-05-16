22e03f10b79bee9ae5383c793ae3bbd08a7d2e9b
gustavoloyola <gustavoloyola287@gmail.com>
2024-05-16 06:51:03 -0300

    nuevas carpetas

--- ejercicio4.py
+++
@@ -1,6 +0,0 @@
-#contador de palabras
-frase = input("Ingrese una frase: ")
-
-palabra = frase.split()
-numPalabras = len(palabra)
-print("La cantidad de palabras es: ", numPalabras)
--- funciones/convertirEspacio.py
+++
@@ -1,7 +0,0 @@
-#convertir una frase 
-print("Convertidor de texto a texto y espacio")
-saludo = input("ingrese su nombre: ")
-print("hola,", saludo )
-frase = input("ingrese una frase: ")
-separador = frase[]
-
